dff7d50c14b649f6d1de7052e8d6439ad5ba2f01
Abe Voelker <abe@abevoelker.com>
2012-06-05 22:17:35 -0500

    10.2B release

--- lib/jdbc/openedge.rb
+++ lib/jdbc/openedge.rb
@@ -1,13 +1,12 @@
 module Jdbc
   module OpenEdge
-    VERSION = "10.1C"
+    VERSION = "10.2B"
   end
 end
 
 if RUBY_PLATFORM =~ /java/
-  require 'base.jar'
   require 'openedge.jar'
-  require 'util.jar'
+  require 'pool.jar'
 elsif $VERBOSE
   warn "jdbc-OpenEdge is only for use with JRuby!"
 end
