f2ef16b57c1d1b7ed0bd61e4fdbfa8832c88b66d
Abe Voelker <abe@abevoelker.com>
2012-06-06 19:47:10 -0500

    Improve missing .jar error message

--- lib/jdbc/openedge.rb
+++ lib/jdbc/openedge.rb
@@ -1,12 +1,22 @@
 module Jdbc
   module OpenEdge
     VERSION = "10.2B"
+    JARS = [ 'openedge.jar', 'pool.jar' ]
   end
 end
 
 if RUBY_PLATFORM =~ /java/
-  require 'openedge.jar'
-  require 'pool.jar'
+  Jdbc::OpenEdge::JARS.each do |j|
+    begin
+      require j
+    rescue LoadError
+      raise LoadError, <<-HERE.gsub!(/\s\s/, '')
+        Unable to load #{j}. Make sure that you've
+        put the driver .jars in your Java $CLASSPATH!
+      HERE
+    end
+  end
+
 elsif $VERBOSE
   warn "jdbc-OpenEdge is only for use with JRuby!"
 end
